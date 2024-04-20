670de6204e4d9167988b966d157f8a38bf55f10a
RandyHuynh5815 <randyhuynh5815@gmail.com>
2024-04-19 22:26:25 -0700

    Added home page skeleton

--- lahacks2024/src/pages/Home.jsx
+++ lahacks2024/src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import { Link } from "react-router-dom"
+import {MapContainer, TileLayer} from "react-leaflet" 
+import "leaflet/dist/leaflet.css"
+import "./Home.scss"
 
 export function Home() {
     return (
-        <>
-            <h1>Home Page</h1>
-            <Link to="/">Log Off</Link>
-        </>
+        <div className="home">
+            <div className="header">Pokemon Go Touch Some Grass</div>
+            <div className="home-main">
+                <div className="filter">Filter</div>
+                <MapContainer center={[33.645887, -117.842694]} zoom={17}>
+                    <TileLayer 
+                        attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+                        url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
+                    />
+                </MapContainer>
+                <div className="description">Description</div>
+            </div>
+        </div>
     )
 }
