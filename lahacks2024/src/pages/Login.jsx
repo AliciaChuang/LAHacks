c40f25a9b4e7ad36d12f7538dd7f37092d4af1d2
RandyHuynh5815 <randyhuynh5815@gmail.com>
2024-04-19 20:56:42 -0700

    Setup pages

---
+++ lahacks2024/src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom"
+export function Login() {
+    return (
+        <>
+            <h1>Login Page</h1>
+            <Link to="/home">Login</Link>
+        </>
+    )
+}
