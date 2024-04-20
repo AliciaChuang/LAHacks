8c97db554fad0ba8f96e75cb85d7cdd337243591
RandyHuynh5815 <randyhuynh5815@gmail.com>
2024-04-20 00:16:18 -0700

    Set map center to current location

--- lahacks2024/src/pages/Home.jsx
+++ lahacks2024/src/pages/Home.jsx
@@ -1,14 +1,36 @@
-import {MapContainer, TileLayer} from "react-leaflet" 
+import {MapContainer, TileLayer, useMap} from "react-leaflet" 
 import "leaflet/dist/leaflet.css"
 import "./Home.scss"
+import { useEffect, useState } from "react"
 
 export function Home() {
+    const [latitude, setLatitude] = useState(33.645887)
+    const [longitude, setLongitude] = useState(-117.842694)
+
+    const Recenter = () => {
+        const map = useMap();
+
+        useEffect(() => {
+            navigator.geolocation.getCurrentPosition(function (position) {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);
+                map.panTo([latitude, longitude]);
+            }, function (e) {
+                //Your error handling here
+            }, {
+                enableHighAccuracy: true
+            });
+        });
+        return null;
+    }
+
     return (
         <div className="home">
             <div className="header">Pokemon Go Touch Some Grass</div>
             <div className="home-main">
                 <div className="filter">Filter</div>
-                <MapContainer center={[33.645887, -117.842694]} zoom={17}>
+                <MapContainer center={[latitude, longitude]} zoom={17}>
+                    <Recenter />
                     <TileLayer 
                         attribution='&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
                         url='https://tile.openstreetmap.org/{z}/{x}/{y}.png'
